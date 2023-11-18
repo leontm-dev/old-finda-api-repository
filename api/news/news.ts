f027023edd59f120a4301fbe407a7c4b917dc16a
Leon™ <leontm.dev@gmail.com>
2023-11-18 15:27:27 +0100

    Finished news router

--- api/news/news.ts
+++ api/news/news.ts
@@ -8,7 +8,23 @@ const newsRouter = Express.Router();
 
 // API-Imports
 
+import changeAuthorRouter from "./change-author";
+import createRouter from "./create";
+import deleteRouter from "./delete";
+import editDateRouter from "./edit-date";
+import editDescriptionRouter from "./edit-description";
+import editTitleRouter from "./edit-title";
+import getRouter from "./get";
+
 // Routes
+
+newsRouter.use("/change-author", changeAuthorRouter);
+newsRouter.use("/create", createRouter);
+newsRouter.use("/delete", deleteRouter);
+newsRouter.use("/edit-date", editDateRouter);
+newsRouter.use("/edit-description", editDescriptionRouter);
+newsRouter.use("/edit-title", editTitleRouter);
+newsRouter.use("/get", getRouter);
 
 // Exports
 
