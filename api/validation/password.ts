3c40d1390f4ff86872ac29203ee447fd907adec6
Leon™ <leontm.dev@gmail.com>
2023-11-18 15:23:56 +0100

    Added password req

--- api/validation/password.ts
+++ api/validation/password.ts
@@ -9,6 +9,14 @@ const passwordRouter = Express.Router();
 
 // Code
 
+passwordRouter.get("/password=:password", (req, res) => {
+  if (process.env["KEY"] === req.params.password) {
+    res.status(200);
+  } else {
+    res.status(401);
+  }
+});
+
 // Exports
 
 export default passwordRouter;
